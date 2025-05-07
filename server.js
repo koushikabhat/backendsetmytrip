88aa08d714a0647b8b6fbe59f6bdecfaabee365f
koushik <koushi@gmail.com>
2025-05-07 22:58:32 +0530

    Updates code after the env created

--- server.js
+++ server.js
@@ -6,7 +6,25 @@ const { GoogleGenAI } = require('@google/genai')
 const cors = require('cors')
 const admin = require('firebase-admin')
 
-const serviceAccount = require(process.env.GOOGLE_APPLICATION_CREDENTIALS); // Firebase service account key
+// const serviceAccount = require(process.env.GOOGLE_APPLICATION_CREDENTIALS); 
+
+const serviceAccount = {
+  type: 'service_account',
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g,'\n'),
+  // private_key: JSON.parse(`"${process.env.PRIVATE_KEY}"`),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+  universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN
+};
+
+
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
